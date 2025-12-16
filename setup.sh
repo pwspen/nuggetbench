b9b34fb54eb344ff52e817f3020bf2da57d39bbd
Patrick Spencer <patrickwspencer@gmail.com>
2025-12-16 10:15:37 -0500

    updates

---
+++ setup.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+  set -euo pipefail
+
+  remote_url="https://github.com/pwspen/imgbench-core.git"
+
+  if git remote get-url core >/dev/null 2>&1; then
+    git remote set-url core "$remote_url"
+  else
+    git remote add core "$remote_url"
+  fi
+
+  git config alias.core-pull 'subtree pull --prefix core core main'
+  git config alias.core-push 'subtree push --prefix core core main'
